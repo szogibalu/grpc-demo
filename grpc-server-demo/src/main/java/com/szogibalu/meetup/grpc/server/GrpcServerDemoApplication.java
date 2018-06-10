975eefcb0e98237110e7aa0738333fe47076102b
Balázs Szögi <szogibalu@gmail.com>
2018-06-10 18:17:25 +0200

    Init project

---
+++ grpc-server-demo/src/main/java/com/szogibalu/meetup/grpc/server/GrpcServerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.szogibalu.meetup.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerDemoApplication.class, args);
+    }
+}
---
+++ grpc-server-demo/src/test/java/com/szogibalu/meetup/grpc/server/GrpcServerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szogibalu.meetup.grpc.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrpcServerDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
